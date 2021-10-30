21750a693cf10073006891f52a8bbd207598d209
Селина Мария <mselina@sfedu.ru>
2021-10-30 11:00:23 +0300

    Revert "Добавлена функция SecondsInMinute"
    This reverts commit 527d628e0aa91deed0d0a0f97db1030039364c5a.

--- lesson15/lesson15/main.cpp
+++ lesson15/lesson15/main.cpp
@@ -1,15 +1,2 @@
-
-//Функция определяет високосный ли год
-bool IsLeapYear(int y)
-{
-	return (y % 400 != 0 && y % 100 == 0) ? 0 : (y % 4 == 0) ? 1 : 0;
-}
-
-int SecondsInMinute()
-{
-	return 60;
-}
-
-
 int main()
 {}
